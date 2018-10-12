543070418feeeaf72da6fab9dea17dda8aa9088c
Nyah Check <check.nyah@gmail.com>
2018-10-12 11:50:39 -0700

    Create handler func

---
+++ handler.go
@@ -0,0 +1,28 @@
+package main
+
+import (  
+    "fmt"
+
+    "github.com/aws/aws-lambda-go/events"
+    "github.com/aws/aws-lambda-go/lambda"
+)
+
+func Handler(request events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+
+    fmt.Printf("events.APIGatewayProxyRequestContext.Identity: %#v \n", request.RequestContext)
+
+    fmt.Printf("Headers: %#v \n", request.Headers)
+
+    return events.APIGatewayProxyResponse{
+        Body:       "API Gateway call, authenticated",
+        StatusCode: 200,
+        Headers: map[string]string{
+            "Access-Control-Allow-Origin":      "*",
+            "Access-Control-Allow-Credentials": "true",
+        },
+    }, nil
+}
+
+func main() {  
+    lambda.Start(Handler)
+}
